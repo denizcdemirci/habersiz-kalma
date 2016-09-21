2464cc4d3b33b290d0e0690095763d86ec65a109
riza <rizasabuncu@outlook.com>
2016-09-21 11:43:21 +0300

    API or network failure status

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 (function() {
   var articles = [];
   $.get("http://www.ntv.com.tr/turkiye.rss", function(data) {
+
+  }).done(function(data) {
     var $xml = $(data);
     $xml.find("entry").each(function() {
       var $this = $(this),
-        item = {
-          title: $this.find("title").text(),
-          description: $this.find("summary").text(),
-          image: $this.find("content").text(),
-          link: $this.find("id").text()
-        }
+      item = {
+        title: $this.find("title").text(),
+        description: $this.find("summary").text(),
+        image: $this.find("content").text(),
+        link: $this.find("id").text()
+      }
       articles.push ({
         title: item.title,
         description: item.description,
@@ -18,6 +20,8 @@
       });
     });
     article();
+  }).fail(function() {
+    // failure 
   });
   function article() {
     var article = articles[Math.floor(Math.random() * articles.length)];
@@ -26,10 +30,10 @@
     var regExp = /\ src="(.*?)\?width/;
     var articleImage = regExp.exec(article.image);
     document.getElementById("article").innerHTML =
-      '<p class="date">' + date.getDate() + ' ' + monthNames[date.getMonth()] + ' ' + date.getFullYear() + ' Türkiye gündeminden bir kesit</p>' +
-      '<p class="title">' + article.title + '</p>' +
-      '<p class="description">' + article.description + '</p>' +
-      '<p class="more"><a href=' + article.link + '>Bu haberin devamını okumak istiyorum</a></p>';
+    '<p class="date">' + date.getDate() + ' ' + monthNames[date.getMonth()] + ' ' + date.getFullYear() + ' Türkiye gündeminden bir kesit</p>' +
+    '<p class="title">' + article.title + '</p>' +
+    '<p class="description">' + article.description + '</p>' +
+    '<p class="more"><a href=' + article.link + '>Bu haberin devamını okumak istiyorum</a></p>';
     document.body.style.backgroundImage = 'url('+ articleImage[1] +')';
   }
 })();
